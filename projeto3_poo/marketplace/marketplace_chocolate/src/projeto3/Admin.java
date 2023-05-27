d2fb3f82e8e82b6816ffa181653f78123ac377c1
Gustavo Wallace Macedo Santos <80765303+Gustavo-Wallace@users.noreply.github.com>
2023-05-26 21:36:55 -0300

    Delete projeto3_poo/marketplace/marketplace_chocolate directory

--- projeto3_poo/marketplace/marketplace_chocolate/src/projeto3/Admin.java
+++
@@ -1,7 +0,0 @@
-package projeto3;
-
-class Admin extends Usuario {
-    public Admin(String nomeUsuario, String senha) {
-        super(nomeUsuario, senha);
-    }
-}
--- projeto3_poo/marketplace/marketplace_chocolate/src/projeto3/Vendedor.java
+++
@@ -1,7 +0,0 @@
-package projeto3;
-
-class Vendedor extends Usuario {
-    public Vendedor(String nomeUsuario, String senha) {
-        super(nomeUsuario, senha);
-    }
-}
