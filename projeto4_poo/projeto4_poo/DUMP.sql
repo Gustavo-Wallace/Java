6ddfeb71b730724b70b076a30d7767ee0f8a6b9e
Gustavo Wallace Macedo Santos <80765303+Gustavo-Wallace@users.noreply.github.com>
2023-06-18 23:38:52 -0300

    test.sql

--- projeto4_poo/projeto4_poo/DUMP.sql
+++ projeto4_poo/projeto4_poo/DUMP.sql
@@ -1,7 +1,1 @@
-CREATE TABLE `chocolatesbd`.`produto` (
-  `id_produto` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `preco` DOUBLE NOT NULL,
-  `quantidade` INT NOT NULL,
-  `vendedor` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_produto`));
+
